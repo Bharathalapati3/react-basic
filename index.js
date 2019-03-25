b6289c303b46c9f16f542f64ad09a935f1072fad
Bharathalapati3 <bharathalapati03@gmail.com>
2019-03-25 11:42:12 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
@@ -23,4 +23,4 @@ class App extends Component {
   }
 }
 
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
